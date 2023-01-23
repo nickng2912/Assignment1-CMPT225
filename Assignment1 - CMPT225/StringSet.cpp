6963869bea6544a1196da684f444c411c07c4677
nickng2912 <nickng2912@gmail.com>
2023-01-22 16:36:33 -0800

    Just Finish Implementing the Project. Test on WSL work fine but there was a Segmentation fault. Need to work on the Set Operations functions.

--- Assignment1 - CMPT225/StringSet.cpp
+++ Assignment1 - CMPT225/StringSet.cpp
@@ -1,1 +1,215 @@
+#include "StringSet.h"
 #include <iostream> 
+#include <string> 
+
+using std::cout;
+using std::endl;
+using std::string;
+
+StringSet::StringSet()
+{
+	maxSize = 2;
+	currentSize = 0;
+
+	arrStr = new string[maxSize];
+}
+
+StringSet::StringSet(const StringSet& str)
+{
+	maxSize = str.maxSize;
+	currentSize = str.currentSize; 
+
+	arrStr = new string[maxSize];
+
+	for (int i = 0; i < currentSize; i++)
+	{
+		arrStr[i] = str.arrStr[i];
+	}
+}
+
+StringSet::~StringSet()
+{
+	delete[] arrStr;
+}
+
+StringSet& StringSet::operator= (const StringSet& str)
+{
+	if (this != &str)
+	{
+		delete[] arrStr; 
+		maxSize = str.maxSize;
+		currentSize = str.currentSize; 
+
+		arrStr = new string[maxSize];
+		for (int i = 0; i < currentSize; i++)
+		{
+			arrStr[i] = str.arrStr[i];
+		}
+	}
+	return *this; 
+}
+
+
+bool StringSet::insert(string value)
+{
+	// Check if the value is already in the set
+	for (int i = 0; i < currentSize; i++)
+	{
+		if (arrStr[i] == value)
+			return false; 
+	}
+
+	//Check if the array is full, if yes then we double the maximum size
+	if (currentSize == maxSize)
+	{
+		doubleTheSize();
+	}
+
+	arrStr[currentSize] = value; 
+	currentSize++;
+	return true;
+}
+
+void StringSet::remove(string value)
+{
+	for (int i = 0; i < currentSize; i++)
+	{
+		if (arrStr[i] == value)
+		{
+			arrStr[i] = arrStr[currentSize - 1];
+			currentSize--; 
+		}
+	}
+}
+
+int StringSet::find(string value) const
+{
+	for (int i = 0; i < currentSize; i++)
+	{
+		if (arrStr[i] == value)
+			return i; 
+	}
+	return -1; 
+}
+
+int StringSet::size() const
+{
+	return currentSize; 
+}
+
+
+StringSet StringSet::ssUnion(const StringSet& str) const
+{
+	StringSet temp; 
+
+	for (int i = 0; i < this->currentSize; i++)
+	{
+		temp.insert(this->arrStr[i]);
+	}
+	for (int i = 0; i < str.currentSize; i++)
+	{
+		temp.insert(str.arrStr[i]);
+	}
+	
+	return temp;
+}
+
+StringSet StringSet::intersection(const StringSet& str) const
+{
+	StringSet temp; 
+
+	for (int i = 0; i < this->currentSize; i++)
+	{
+		if (str.find(this->arrStr[i]) != -1)
+			temp.insert(this->arrStr[i]);
+	}
+
+	for (int i = 0; i < str.currentSize; i++)
+	{
+		if (this->find(str.arrStr[i]) != -1)
+		{
+			temp.insert(str.arrStr[i]);
+		}
+	}
+	
+	return temp; 
+}
+
+StringSet StringSet::difference(const StringSet& str) const
+{
+	StringSet temp;
+
+	for (int i = 0; i < this->currentSize; i++)
+	{
+		if (str.find(this->arrStr[i]) == -1)
+			temp.insert(this->arrStr[i]);
+	}
+
+	for (int i = 0; i < str.currentSize; i++)
+	{
+		if (this->find(str.arrStr[i]) == -1)
+		{
+			temp.insert(str.arrStr[i]);
+		}
+	}
+
+	return temp;
+}
+
+
+//Helper Function 
+void StringSet::doubleTheSize()
+{
+	maxSize *= 2; 
+	string* tempArray = new string[maxSize];
+	
+	for (int i = 0; i < currentSize; i++)
+	{
+		tempArray[i] = arrStr[i];
+	}
+	
+	delete[] arrStr; 
+	arrStr = tempArray;
+
+	delete[] tempArray;
+}
+
+
+int main() {
+
+	StringSet sset1;
+	sset1.insert("cat");
+	sset1.insert("bat");
+	sset1.insert("rat");
+	bool insertTest = sset1.insert("badger");
+	cout << "insert success: " << insertTest << endl;
+
+	StringSet sset2;
+	sset2.insert("elephant");
+	sset2.insert("bat");
+	sset2.insert("hamster");
+	sset2.insert("weasel");
+	sset2.remove("weasel");
+	int findTest = sset2.find("weasel");
+	cout << "index of weasel = " << findTest << endl;
+
+	// Use the copy constructor to build a StringSet with sset1 U sset2
+	StringSet sset3(sset1.ssUnion(sset2));
+
+	// Use overloaded assignment operator to make a StringSet with sset2 int sset1
+	StringSet sset4;
+	sset4 = sset2.intersection(sset1);
+	// Use overloaded assignment operator to make a StringSet with sset2 - sset1
+	sset4 = sset2.difference(sset1);
+
+	cout << "end basic test" << endl;
+	
+	return 0; 
+}
+
+
+
+
+
+
+
--- Assignment1 - CMPT225/StringSet.h
+++ Assignment1 - CMPT225/StringSet.h
@@ -4,28 +4,47 @@ using std::string;
 class StringSet
 {
 public: 
-	//Constructors and Destructor 
+	//Default Constructor 
 	StringSet(); 
+
+	//Copy Constructor
 	StringSet(const StringSet& str);
+
+	//Destructor
 	~StringSet(); 
 
 	//Overloaded Assignment Operator 
 	StringSet& operator = (const StringSet& str); 
 
-	//Behaviours (Functions) 
-	bool insert(string value);
-	void remove(string value); 
-	int find(string value); 
 
+	// return fales if the value is already in the string set, otherwise add the value to the set
+	bool insert(string value);
+
+	// remove the value that matches in the string set, and then decrement the current size
+	void remove(string value); 
+
+	// return index of the value that match in the string set
+	int find(string value) const; 
+
+	// return the size of the set
 	int size() const; //getters 
 
-	StringSet ssUnion(const StringSet& str);
-	StringSet intersection(const StringSet& str);
-	StringSet difference(const StringSet& str); 
+	
+	// return a StringSet object that contains the union of the calling object and the str
+	StringSet ssUnion(const StringSet& str) const;
+
+	//return a StringSet object that contains the intersection of the calling object and the str
+	StringSet intersection(const StringSet& str) const;
+
+	//return a Stringset object that contains the set difference of the calling object and the str
+	StringSet difference(const StringSet& str) const; 
 
 private: 
 	//Attributes 
 	string* arrStr;
 	int currentSize; 
 	int maxSize; 
+
+	//Helper function 
+	void doubleTheSize();
 };
